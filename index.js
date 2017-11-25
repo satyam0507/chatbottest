92a16528de8dca57662fb0728c59d9d2d3414250
satyam0507 <satyam0507@gmail.com>
2017-11-25 15:37:16 +0530

    webhook code changed

--- index.js
+++ index.js
@@ -15,13 +15,33 @@ app.get('/',(req,res)=>{
     res.send('hi i am bot');
 });
 
-app.get('/webhook/',(req,res)=>{
-    if(req.query['hub.veify_token']==='satyamTest'){
-        res.send(req.query['hub.challenge']);
-    }else{
-        res.send('Wrong token');
-    }
-});
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+    
+      // Your verify token. Should be a random string.
+      let VERIFY_TOKEN = "satyamTest"
+        
+      // Parse the query params
+      let mode = req.query['hub.mode'];
+      let token = req.query['hub.verify_token'];
+      let challenge = req.query['hub.challenge'];
+        
+      // Checks if a token and mode is in the query string of the request
+      if (mode && token) {
+      
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+          
+          // Responds with the challenge token from the request
+          console.log('WEBHOOK_VERIFIED');
+          res.status(200).send(challenge);
+        
+        } else {
+          // Responds with '403 Forbidden' if verify tokens do not match
+          res.sendStatus(403);      
+        }
+      }
+    });
 
 app.listen(app.get('port'),function(){
     console.log('running on port :' +app.get('port'));
