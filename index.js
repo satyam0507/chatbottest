8e4cf68c4308310e9412b3f5fa37fa1c17fb85a4
satyam0507 <satyam0507@gmail.com>
2017-11-25 14:55:06 +0530

    initial commite

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.set('port',(process.env.PORT||5000));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.send('hi i am bot');
+});
+
+app.get('/webhook/',(req,res)=>{
+    if(req.query['hub.veify_token']==='blondiebytes'){
+        res.send(req.query['hub.challenge']);
+    }else{
+        res.send('Wrong token');
+    }
+});
+
+app.listen(app.get('port'),function(){
+    console.log('running on port :' +app.get('port'));
+});
